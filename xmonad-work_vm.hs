2ffb5b229ce1be3711aed5568bd57475e91f84c7
Niklas Thorne <niklas.thorne@se.transport.bombardier.com>
2013-04-26 09:33:38 +0200

    Added keybinding for logging on to chat

--- xmonad-work_vm.hs
+++ xmonad-work_vm.hs
@@ -36,6 +36,7 @@ myKeys =
   , ("M-S-l", spawn "xterm -ls -e ssh -t interflo@lemmel 'cd Users/nthorne ; /bin/bash -l'")
   , ("M-S-h", spawn "xterm -ls -e ssh interflo@hermelin")
   , ("M-S-t", spawn "xterm -ls -e 'source current_project.zsh && test -n $CURRENT_PROJECT_ROOT && cd $CURRENT_PROJECT_ROOT; /usr/bin/zsh'")
+  , ("M-S-i", spawn "xterm -ls -e ssh -t gbguxs04 'bin/irssi -c localhost'")
   ]
 
 myWorkspaces = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
