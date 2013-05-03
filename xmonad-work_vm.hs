345337a50ff43f513d9f0d84c2958578de4fabf1
Niklas Thorne <niklas.thorne@se.transport.bombardier.com>
2013-05-03 09:40:21 +0200

    Added hotkey for opening terminals in source tree

--- xmonad-work_vm.hs
+++ xmonad-work_vm.hs
@@ -37,7 +37,8 @@ myKeys =
   , ("M-S-g", spawn "xterm -ls -e ssh -t gbguxs10 'source current_project.zsh ; test -n $CURRENT_PROJECT_ROOT && cd $CURRENT_PROJECT_ROOT ; /usr/bin/zsh -l'")
   , ("M-S-l", spawn "xterm -ls -e ssh -t interflo@lemmel 'cd Users/nthorne ; /bin/bash -l'")
   , ("M-S-h", spawn "xterm -ls -e ssh interflo@hermelin")
-  , ("M-S-t", spawn "xterm -ls -e 'source current_project.zsh && test -n $CURRENT_PROJECT_ROOT && cd $CURRENT_PROJECT_ROOT; /usr/bin/zsh'")
+  , ("M-S-a", spawn "xterm -ls -e 'source current_project.zsh && test -n $CURRENT_PROJECT_ROOT && cd $CURRENT_PROJECT_ROOT; /usr/bin/zsh'")
+  , ("M-S-t", spawn "xterm -ls -e 'source current_project.zsh && test -n $CURRENT_PROJECT_ROOT && cd $CURRENT_PROJECT_ROOT/Implementation/TCC_SW; /usr/bin/zsh'")
   , ("M-S-i", spawn "xterm -ls -e ssh -t gbguxs04 'bin/irssi -c localhost'")
   ]
 
