1a18f9b55c8834342dd24afa7a7c39f252be41bb
Niklas Thorne <notrupertthorne@gmail.com>
2014-01-18 21:38:55 +0100

    Status bar and hotkeys

--- xmonad-home.hs
+++ xmonad-home.hs
@@ -3,28 +3,31 @@ import XMonad.Config.Gnome
 import XMonad.Hooks.DynamicLog
 import XMonad.Hooks.ManageDocks
 import XMonad.Util.EZConfig
+import XMonad.Util.Run
 
 myManageHook = composeAll
     [ title =? "Run Application" --> doFloat  -- cause the app runner to float
     ]
 
-myLogHook = dynamicLogWithPP $ defaultPP
 
 main = do
+  dzenLeftBar <- spawnPipe "dzen2 -x '0' -w '770' -ta 'l'"
+  dzenRightBar <- spawnPipe "conky | dzen2 -x '770' -w '770' -ta 'r'"
   xmonad $ gnomeConfig
     { manageHook = manageDocks <+> myManageHook
 		    <+> manageHook gnomeConfig
     , workspaces = myWorkspaces
     , modMask = mod4Mask
     , terminal = "xterm"
-    , logHook = myLogHook
+    , logHook = dynamicLogWithPP $ defaultPP { ppOutput = hPutStrLn dzenLeftBar }
     }
     `additionalKeysP` myKeys
 
 
 myKeys =
-  [ ("M-S-q", spawn "gnome-session-quit")
-  , ("M-S-h", spawn "gnome-session-quit --power-off")
+  [ ("M-S-q", spawn "pkill xmonad")
+  , ("M-S-h", spawn "gksudo pm-hibernate")
+  , ("M-S-d", spawn "notify-send -t 5000 \"$(date)\"")
   , ("M-z", spawn "gnome-screensaver-command --lock") 
   , ("M-p", spawn "dmenu_run")
   , ("M-S-f", spawn "firefox")
