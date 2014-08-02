1922dc9e54c4ba5305a36bfa58887950ba0b9893
Niklas Thorne <notrupertthorne@gmail.com>
2014-08-02 23:19:56 +0200

    Added window copying

--- xmonad-home.hs
+++ xmonad-home.hs
@@ -4,19 +4,41 @@ import XMonad.Hooks.DynamicLog
 import XMonad.Hooks.ManageDocks
 import XMonad.Util.EZConfig
 import XMonad.Util.Run
+import XMonad.Actions.CopyWindow
+import XMonad.Prompt
+import XMonad.Prompt.Workspace
 
 myManageHook = composeAll
     [ title =? "Run Application" --> doFloat  -- cause the app runner to float
     ]
 
+-- Main pallete
+colLight = "#d8d8d8"
+colDark = "#343d55"
+colVeryDark = "#000000"
+colTextLight = "#ffffff"
+colTextDark = "#bbbbbb"
+colBorderLight = colLight
+colBorderDark = colDark
+
+-- Same font as for a console
+myFont = "-xos4-terminus-medium-r-*-*-14-*-*-*-*-*-*-*"
+
+myXPConfig :: XPConfig
+myXPConfig = defaultXPConfig
+  { font        = myFont
+  , bgColor     = colLight
+  , fgColor     = colVeryDark
+  , borderColor = colLight
+  }
 
 main = do
-  dzenLeftBar <- spawnPipe "dzen2 -x '0' -w '770' -ta 'l'"
-  dzenRightBar <- spawnPipe "conky | dzen2 -x '770' -w '770' -ta 'r'"
+  dzenLeftBar <- spawnPipe ("dzen2 -x '0' -w '770' -ta 'l' -fn " ++ myFont)
+  dzenRightBar <- spawnPipe ("conky | dzen2 -x '770' -w '770' -ta 'r' -fn " ++ myFont)
   xmonad $ gnomeConfig
     { manageHook = manageDocks <+> myManageHook
 		    <+> manageHook gnomeConfig
-    , workspaces = myWorkspaces
+    , XMonad.workspaces = myWorkspaces
     , modMask = mod4Mask
     , terminal = "xterm"
     , logHook = dynamicLogWithPP $ defaultPP { ppOutput = hPutStrLn dzenLeftBar }
@@ -27,10 +49,11 @@ main = do
 myKeys =
   [ ("M-S-q", spawn "pkill xmonad")
   , ("M-S-h", spawn "gksudo pm-hibernate")
-  , ("M-S-d", spawn "notify-send -t 5000 \"$(date)\"")
   , ("M-z", spawn "gnome-screensaver-command --lock") 
-  , ("M-p", spawn "dmenu_run")
+  , ("M-p", spawn ("dmenu_run -fn " ++ myFont))
   , ("M-S-f", spawn "firefox")
+  , ("M-d", workspacePrompt myXPConfig (windows . copy))
+  , ("M-S-d", killAllOtherCopies)
   ]
 
 myWorkspaces = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
