0650774e77e42f49e5def09be7dce8f654a46498
Niklas Thörne <notrupertthorne@gmail.com>
2017-06-03 22:32:35 +0200

    Resized dzen bar and cleaned up config

--- xmonad-nixos.hs
+++ xmonad-nixos.hs
@@ -1,22 +1,22 @@
 import XMonad
+import XMonad.Util.EZConfig (additionalKeysP, additionalMouseBindings)
+import XMonad.Hooks.DynamicLog (ppOutput, dynamicLogWithPP, defaultPP)
 import XMonad.Hooks.ManageDocks
-import XMonad.Util.EZConfig
-import XMonad.Hooks.DynamicLog
-import XMonad.Hooks.DynamicLog (dzen)
-import XMonad.Hooks.ManageDocks
-import XMonad.Layout.Tabbed
+import XMonad.Layout.Tabbed (simpleTabbed)
 import XMonad.Layout.Grid
 import XMonad.Layout.PerWorkspace (onWorkspaces)
 import qualified XMonad.Actions.FlexibleResize as Flex
-import XMonad.Actions.CopyWindow
+import XMonad.Actions.CopyWindow (copy, killAllOtherCopies)
 import XMonad.Prompt
 import XMonad.Prompt.Workspace
 import XMonad.Prompt.Shell
 import XMonad.Util.Run
 
+
 myManageHook = composeAll
     [ title =? "Run Application" --> doFloat  -- cause the app runner to float
     ]
+
 
 -- Main pallete
 colLight = "#d8d8d8"
@@ -27,15 +27,17 @@ colTextDark = "#bbbbbb"
 colBorderLight = colLight
 colBorderDark = colDark
 
+
 -- Same font as for a console
 myFont = "-xos4-terminus-medium-r-*-*-14-*-*-*-*-*-*-*"
 
-myXPConfig :: XPConfig
+
 myXPConfig = defaultXPConfig
   { bgColor     = colLight
   , fgColor     = colVeryDark
   , borderColor = colLight
   }
+
 
 myDefaultLayout = avoidStruts (Grid ||| Mirror tiled ||| tiled ||| simpleTabbed) ||| Full
   where
@@ -44,6 +46,7 @@ myDefaultLayout = avoidStruts (Grid ||| Mirror tiled ||| tiled ||| simpleTabbed)
     delta = 3/100
     ratio = 1/2
 
+
 myCustomSizedLayout = avoidStruts (Mirror tiled ||| tiled ||| simpleTabbed ||| Full ||| Grid)
   where
     tiled = Tall nmaster tiledDelta tiledRatio
@@ -51,27 +54,9 @@ myCustomSizedLayout = avoidStruts (Mirror tiled ||| tiled ||| simpleTabbed ||| F
     tiledDelta = 3/100
     tiledRatio = 1/5
 
+
 myLayoutHook = onWorkspaces ["1", "2", "3"] myCustomSizedLayout $
   myDefaultLayout
-
-	
-
-
-main = do
-  -- dzenLeftBar <- spawnPipe ("dzen2 -dock -x '0' -ta 'l' -fn " ++ myFont)
-  dzenLeftBar <- spawnPipe ("dzen2 -dock -x '0' -w '700' -ta 'l' -fn " ++ myFont)
-  dzenRightBar <- spawnPipe ("conky | dzen2 -dock -x '700' -w '770' -ta 'r' -fn " ++ myFont)
-  xmonad $ defaultConfig
-    { manageHook = manageDocks <+> myManageHook
-		     -- <+> manageHook defaultConfig
-    , XMonad.workspaces = myWorkspaces
-    , terminal = "xterm"
-    , layoutHook = myLayoutHook
-    , logHook = dynamicLogWithPP $ defaultPP { ppOutput = hPutStrLn dzenLeftBar }
-    , handleEventHook = docksEventHook
-    }
-    `additionalKeysP` myKeys
-    `additionalMouseBindings` myMouse
 
 
 myKeys =
@@ -88,4 +73,22 @@ myKeys =
 myMouse =
   [ ((shiftMask, button1), (\w -> focus w >> Flex.mouseResizeWindow w))]
 
+
 myWorkspaces = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
+
+
+main = do
+  -- dzenLeftBar <- spawnPipe ("dzen2 -dock -x '0' -ta 'l' -fn " ++ myFont)
+  dzenLeftBar <- spawnPipe ("dzen2 -dock -x '0' -w '600' -ta 'l' -fn " ++ myFont)
+  dzenRightBar <- spawnPipe ("conky | dzen2 -dock -x '600' -w '766' -ta 'r' -fn " ++ myFont)
+  xmonad $ defaultConfig
+    { manageHook = manageDocks <+> myManageHook
+		     -- <+> manageHook defaultConfig
+    , XMonad.workspaces = myWorkspaces
+    , terminal = "xterm"
+    , layoutHook = myLayoutHook
+    , logHook = dynamicLogWithPP $ defaultPP { ppOutput = hPutStrLn dzenLeftBar }
+    , handleEventHook = docksEventHook
+    }
+    `additionalKeysP` myKeys
+    `additionalMouseBindings` myMouse
