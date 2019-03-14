a450abde492d17573b060a1ea21bdcb17a093072
Niklas Thorne <niklas.thorne@aptiv.com>
2019-03-14 13:21:23 +0100

    Findings after fresh install

---
+++ install-work.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+
+# vim: filetype=sh
+
+# Set IFS explicitly to space-tab-newline to avoid tampering
+IFS=' 	
+'
+
+
+NAME=
+# Make sure that we're not on NixOS; if so, avoid tampering with PATH
+if [[ -f /etc/os-release ]]
+then
+  . /etc/os-release
+fi
+
+if [[ "NixOS" != "$NAME" ]]
+then
+  # If found, use getconf to constructing a reasonable PATH, otherwise
+  # we set it manually.
+  if [[ -x /usr/bin/getconf ]]
+  then
+    PATH=$(/usr/bin/getconf PATH)
+  else
+    PATH=/bin:/usr/bin:/usr/local/bin
+  fi
+fi
+
+function error()
+{
+  echo "Error: $@" >&2
+  exit 1
+}
+
+ln -s ${PWD}/xmonad-nixos_work_vm.hs ${HOME}/.xmonad/xmonad.hs
+xmonad --recompile
